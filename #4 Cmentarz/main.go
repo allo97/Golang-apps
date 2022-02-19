129780debab8ffee7572771ca79b1b1e98a82c72
Alek <slodczyk97@gmail.com>
2022-02-19 17:10:03 +0100

    separate loop for every goroutine

--- #4 Cmentarz/main.go
+++ #4 Cmentarz/main.go
@@ -25,27 +25,17 @@ func main() {
 
 	fmt.Println("Uruchamiam babki")
 
-	wg.Add(1)
-	go func() {
-		for len(magazyn.znicze) != 0 && len(magazyn.wiazanki) != 0 {
-			wg.Add(4)
-			go babka1.pobierzProduktZMagazynu(magazyn.znicze, koszNaZnicze, &wg)
-			go babka2.pobierzProduktZMagazynu(magazyn.znicze, koszNaZnicze, &wg)
-			go babka3.pobierzProduktZMagazynu(magazyn.wiazanki, koszNaWiazanki, &wg)
-			go babka4.pobierzProduktZMagazynu(magazyn.wiazanki, koszNaWiazanki, &wg)
-			time.Sleep(time.Second / 100)
-		}
-		defer wg.Done()
-	}()
+	wg.Add(4)
+	go babka1.pobierzProduktZMagazynu(magazyn.znicze, koszNaZnicze, &wg)
+	go babka2.pobierzProduktZMagazynu(magazyn.znicze, koszNaZnicze, &wg)
+	go babka3.pobierzProduktZMagazynu(magazyn.wiazanki, koszNaWiazanki, &wg)
+	go babka4.pobierzProduktZMagazynu(magazyn.wiazanki, koszNaWiazanki, &wg)
 
 	fmt.Println("Uruchamiam poslancow")
 
-	for len(magazyn.znicze) != 0 && len(magazyn.wiazanki) != 0 {
-		for _, poslaniec := range poslancy {
-			wg.Add(1)
-			go poslaniec.pobierzProduktyZKosza(koszNaZnicze, koszNaWiazanki, &wg)
-		}
-		time.Sleep(time.Second / 100)
+	for _, poslaniec := range poslancy {
+		wg.Add(1)
+		go poslaniec.pobierzProduktyZKosza(koszNaZnicze, koszNaWiazanki, magazyn, &wg)
 	}
 
 	wg.Wait()
@@ -67,26 +57,34 @@ func generateMagazyn(iloscWiazanek int, iloscZniczy int) magazyn {
 }
 
 func (babka babka) pobierzProduktZMagazynu(magazyn chan string, kosz chan string, wg *sync.WaitGroup) {
+	for len(magazyn) != 0 {
+		if len(kosz) >= 10 {
+			fmt.Printf("Pełny kosz! %s musi poczekać aż posłaniec odbierze produkt!\n", babka.nazwa)
+		} else {
+			produkt := <-magazyn
+			kosz <- produkt
+			fmt.Printf("%s pobiera %s i dodaje %s do kosza\n", babka.nazwa, babka.typ, produkt)
+		}
+		time.Sleep(time.Second)
 
-	if len(kosz) >= 10 {
-		fmt.Printf("Pełny kosz! %s musi poczekać aż posłaniec odbierze produkt!\n", babka.nazwa)
-	} else {
-		produkt := <-magazyn
-		kosz <- produkt
-		fmt.Printf("%s pobiera %s i dodaje %s do kosza\n", babka.nazwa, babka.typ, produkt)
 	}
 	defer wg.Done()
 }
 
-func (poslaniec poslaniec) pobierzProduktyZKosza(kosz_na_znicze chan string, kosz_na_wiazanki chan string, wg *sync.WaitGroup) {
-	if len(kosz_na_znicze) > 1 && len(kosz_na_wiazanki) > 0 {
-		znicz1 := <-kosz_na_znicze
-		znicz2 := <-kosz_na_znicze
-		wiazanka := <-kosz_na_wiazanki
+func (poslaniec poslaniec) pobierzProduktyZKosza(kosz_na_znicze chan string, kosz_na_wiazanki chan string, magazyn magazyn, wg *sync.WaitGroup) {
+	for len(magazyn.znicze) != 0 || len(magazyn.wiazanki) != 0 {
+		if len(kosz_na_znicze) > 1 && len(kosz_na_wiazanki) > 0 {
+			znicz1 := <-kosz_na_znicze
+			znicz2 := <-kosz_na_znicze
+			wiazanka := <-kosz_na_wiazanki
 
-		fmt.Printf("%s pobiera %s i %s i %s\n", poslaniec.nazwa, znicz1, znicz2, wiazanka)
-	} else {
-		fmt.Printf("Pusty kosz! %s musi poczekać!\n", poslaniec.nazwa)
+			fmt.Printf("%s pobiera %s i %s i %s\n", poslaniec.nazwa, znicz1, znicz2, wiazanka)
+		} else {
+			fmt.Printf("Pusty kosz! %s musi poczekać!\n", poslaniec.nazwa)
+		}
+
+		time.Sleep(time.Second)
+
 	}
 	defer wg.Done()
 }
